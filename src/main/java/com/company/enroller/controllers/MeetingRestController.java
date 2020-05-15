5f6fe7cd737cb9873c82ed1a396d6a73f10267c2
mwarot <mwarot>
2020-05-15 21:44:57 +0200

    .

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -76,7 +76,7 @@ public class MeetingRestController {
 
 	}
 
-	@RequestMapping(value = "/{id}/participants", method = RequestMethod.POST)
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
 	public ResponseEntity<?> addParticipant(@PathVariable("id") long id, @RequestBody Participant participant){
 		Meeting meeting = meetingService.findById(id);
 		if (meeting == null) {
